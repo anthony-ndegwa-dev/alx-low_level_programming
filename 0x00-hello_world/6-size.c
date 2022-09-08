0391acc803d62eea65abfc8510713274912c585e
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-09-08 12:04:37 -0700

    t

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,5 @@
  */
 int main(void)
 {
-	int a;
-	long int b;
-	long long int c;
-	char d;
-	float f;
-
-	printf("Char size: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Int size: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Float size: %lu byte(s)\n", (unsigned long)sizeof(f));
-	return (0);
+	
 }
