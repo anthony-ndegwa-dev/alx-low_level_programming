3d9c2877239b98f78c6561c2e1001cc10a9b7bb8
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-09-09 11:36:49 -0700

    Update variables_if_else_while file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,6 +12,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	/* your code goes there */
 	if (n > 0)
  		printf("%d is positive\n", n);
@@ -19,5 +20,6 @@ int main(void)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
+	
 	return (0);
 }
