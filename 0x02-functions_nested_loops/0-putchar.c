9f1e8343e79a5a77a4785e971b6509bd97a56bf0
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-09-14 10:01:17 -0700

    Create putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf('_putchar\n');
+
+	return (0);
+}
