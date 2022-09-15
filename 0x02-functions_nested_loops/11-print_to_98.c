d55a746f8377d0ff34c43ff9d3dff060a2f4c671
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-09-15 08:35:05 -0700

    Create 11-print_to_98.c file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Prints numbers from input to 98 .
+ * @n: Number to begin counting from.
+ */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
