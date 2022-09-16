d1fbda53502c473a4044eb71ee0fe82766cc6cfc
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-09-16 08:31:53 -0700

    Create 10-print_triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle, using character #.
+ * @size: Size of the triangle.
+ *
+ * Return: Always 0
+ */
+
+void print_triangle(int size)
+{
+
