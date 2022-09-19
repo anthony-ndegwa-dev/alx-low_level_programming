b1cf0d0aeeded3793765668573ed7e3883b81911
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-09-19 01:59:03 -0700

    Create 3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string, and new line to stdout.
+ * @str: Input string pointer
+ *
+ * Return: Always 0
+ */
+
+void _puts(char *str)
+{
+	int x = 0;
+
+	while (str[x] != '\0')
+	{
+		_putchar(str[x]);
+		x++;
+	}
+	_putchar('\n');
+}
