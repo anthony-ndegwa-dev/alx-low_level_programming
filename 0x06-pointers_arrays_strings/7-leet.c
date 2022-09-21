6a6c1be94469b35ff014b7bf70d4defd4e6d46cc
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-09-21 01:11:24 -0700

    Create 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - a function that encodes a string into 1337.
+ * @s: string to encode
+ *
+ * Return: Encode string
+ */
+
+char *leet(char *s)
+{
+	int x, y;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (x = 0; *(s + x); x++)
+	{
+		for (y = 0; y <= 9; j++)
+		{
+			if (a[y] == *(s + x))
+				*(s + x) = b[y];
+		}
+	}
+
+	return (s);
+}
