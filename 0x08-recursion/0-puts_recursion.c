d446f4fe0b73f31375654916e0efe64608c9268a
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-09-29 05:44:20 -0700

    Edit 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,14 +5,13 @@
  * @s: The input string to be printed.
  * Return: Void
  */
-
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		_putchar('\n');
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	else
+		_putchar('\n');
 }
