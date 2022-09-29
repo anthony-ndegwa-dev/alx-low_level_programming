ad7460c2200213fd2fa04c5174d882991808175e
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-09-29 09:12:50 -0700

    Create 3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @n: An input integer
+ * Return: The factorial of n
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n <= 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
