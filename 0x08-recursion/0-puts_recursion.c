98ac24b1d33e7bd9f6cad737bf294ef411eb0cc2
Anthony-ndegwa <anthony.n.ndegwa@gmail.com>
2022-09-29 09:40:36 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,7 +3,7 @@
 /*
  * _puts_recursion - a function that prints a string, followed by a new line.
  * @s: The input string to be printed.
- * Return: Nothing
+ * Return: Void
  */
 void _puts_recursion(char *s)
 {
