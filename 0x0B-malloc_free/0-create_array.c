6d8177a8fc801353f9cf3c0337b7ee198f7bad17
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-10-02 23:51:11 -0700

    Edit 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,19 +12,17 @@
 char *create_array(unsigned int size, char c)
 {
 	char *z;
-	unsigned int i = 0;
-
-	z = malloc(size * sizeof(char));
-
-	if (z == NULL)
-		return (NULL);
+	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
 
-	while (i < size)
-	{
+	z = malloc(sizeof(char) * size);
+
+	if (z == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
 		z[i] = c;
-		i++;
-	}
+
 	return (z);
