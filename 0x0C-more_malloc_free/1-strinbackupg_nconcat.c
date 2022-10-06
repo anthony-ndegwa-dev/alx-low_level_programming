aa4a267ab4f635d4cd96d56a380a5d7f21fffcf7
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-10-06 05:37:35 -0700

    Edit 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-strinbackupg_nconcat.c
+++ 0x0C-more_malloc_free/1-strinbackupg_nconcat.c
@@ -12,32 +12,35 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *concat;
-	unsigned int len = n, index;
+	char *formed_str;
+	unsigned int i = 0, lens1 = 0, lens2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 
+	while (s1[lens1])
+		lens1++;
+
 	if (s2 == NULL)
 		s2 = "";
 
-	for (index = 0; s1[index]; index++)
-		len++;
+	while (s2[lens2])
+		lens2++;
 
-	concat = malloc(sizeof(char) * (len + 1));
+	if (n >= lens2)
+		n = lens2;
 
-	if (concat == NULL)
+	formed_str = malloc(lens1 + n + 1);
+	if (formed_str == NULL)
 		return (NULL);
 
-	len = 0;
+	for (; i < (lens1 + n); i++)
+	{
+		if (i < lens1)
+			formed_str[i] = *s1, s1++;
+		else
+			formed_str[i] = *s2, s2++;
+	}
 
-	for (index = 0; s1[index]; index++)
-		concat[len++] = s1[index];
-
-	for (index = 0; s2[index] && index < n; index++)
-		concat[len++] = s2[index];
-
-	concat[len] = '\0';
-
-	return (concat);
-}
+	formed_str[i] = '\0';
+	return (formed_str);
