6ca8478dddb74dfbb8d97ea3ecfd3aadfd4eb3b4
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-10-11 06:34:33 -0700

    Edit 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_dog - a function that prints a struct dog.
+ * print_dog - function that prints a struct dog.
  * @d: The struct dog to be printed.
  * Return: Nothing.
  */
