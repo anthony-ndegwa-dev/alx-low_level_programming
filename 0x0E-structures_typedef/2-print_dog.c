642e1cdd845ee15c7727beb67b942eab968018c6
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-10-11 00:30:20 -0700

    Create 2-print_dog.c file

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.hi>
+
+/**
+ * print_dog - a function that prints a struct dog.
+ * @d: The struct dog to be printed.
+ * Return: Nothing.
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	
+	if (d->name == NULL)
+		d->name = "(nil)";
+
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %0.6f\nOwner: %s\n", d->name, d->age, d->owner);
+}
