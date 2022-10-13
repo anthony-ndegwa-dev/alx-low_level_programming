5a6439ba1c933b8f9035deef004f4e81a1a91639
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-10-13 06:18:58 -0700

    Edit function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,4 +3,3 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int (*get_op_func(char *s))(int, int);
 int main(int argc, char *argv[]);
-int (*address)(int, char **) = main;
