61af352c2ad4652f18573765507d34e41f47d89a
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-10-13 06:17:01 -0700

    Edit function_pointers.h

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -5,6 +5,7 @@
  * main - Prints the opcodes of itself.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
+ *
  * Return: Always 0.
  */
 
