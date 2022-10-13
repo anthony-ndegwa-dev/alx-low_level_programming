3d2e449fb2fa3b28b32d5d81eddad7d7d4940fb0
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-10-13 05:39:00 -0700

    Edit 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -19,19 +19,24 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(1);
 	}
+
 	bytes = atoi(argv[1]);
+
 	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
+
 	opcode = (unsigned char *)main;
 	i = 0;
+
 	if (bytes > 0)
 	{
 		while (i < (bytes - 1))
 			printf("%02hhx ", opcode[i++]);
 		printf("%hhx\n", opcode[i]);
 	}
+
 	return (0);
 }
