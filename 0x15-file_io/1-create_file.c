26b660645175bd97c02aabb88c54b1f00755741d
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-11-02 11:49:16 -0700

    Modify 1-create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,11 +16,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
 	}
+
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, len);
 	if (o == -1 || w == -1)
