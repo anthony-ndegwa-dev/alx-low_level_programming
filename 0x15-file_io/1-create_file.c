d6ff680a704654fadd11bfbaa9c94c007cad23cd
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-11-02 11:47:37 -0700

    Modify 1-create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,8 +4,8 @@
 
 /**
  * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
+ * @filename: pointer to the name of the file to create.
+ * @text_content: pointer to a string to write to the file.
  *
  * Returns: 1 on success, -1 on failure
  */
@@ -16,19 +16,17 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
 	}
-
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, len);
-
 	if (o == -1 || w == -1)
 		return (-1);
 
 	close(o);
+
 	return (1);
 }
