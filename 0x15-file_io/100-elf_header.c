a0da99d0a815febbbd3856686227c53f20647d12
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-11-03 00:28:17 -0700

    Create 100-elf_header file

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - the entry point for program to get header of ELF file
+ * @argc: the number of arguments
+ * @argv: a pointer to the array of arguments
+ *
+ * Return: 1 on success, error code on failure
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("argc:%d, argv:%p\n", argc, (void *)*argv);
+	return (1);
+}
