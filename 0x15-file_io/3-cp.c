9c1d8213dbeec662f02e06712dda37ab910e9738
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-11-02 23:47:10 -0700

    Modify 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -52,10 +52,10 @@ void close_file(int z)
  *
  * Return: 0 on success.
  *
- * Description: when the argument count is incorrect - exit code 97.
- * 		when file_from does not exist or cannot be read - exit code 98.
- * 		when file_to cannot be created or written to - exit code 99.
- * 		when file_to or file_from cannot be closed - exit code 100.
+ * Description: if argument count is incorrect - exit code 97.
+ * 		if file_from doesn't exist or can't be read - exit code 98.
+ * 		if file_to cannot be created or written to - exit code 99.
+ * 		if file_to or file_from cannot be closed - exit code 100.
  */
 
 int main(int argc, char *argv[])
