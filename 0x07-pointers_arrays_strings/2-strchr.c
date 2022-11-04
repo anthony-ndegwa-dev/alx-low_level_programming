b67c0f02da8fb26e9b184067813c6a4aeac72db2
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-11-04 05:53:41 -0700

    Modify 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,9 +4,9 @@
  * _strchr - function that locates a character in a string.
  * @s: The string to be searched
  * @c: The character to be located
- *
  * Returns: a pointer to the character c in the string s
  */
+
 char *_strchr(char *s, char c)
 {
 	int index;
