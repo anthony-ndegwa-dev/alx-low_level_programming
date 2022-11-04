742d4bd16918ccea6a7dd5112f582d925873d494
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-11-04 07:37:34 -0700

    Modify 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,9 +4,9 @@
  * _memset - fills memory with a constant byte.
  * @s: A pointer to the memory area to be filled.
  * @c: character to fill the memory area with.
- * @n: number of bytes to fill
+ * @n: number of bytes to fill.
  *
- * Return: returns pointer to location filled
+ * Return: pointer to location filled.
  */
 
 char *_memset(char *s, char b, unsigned int n)
