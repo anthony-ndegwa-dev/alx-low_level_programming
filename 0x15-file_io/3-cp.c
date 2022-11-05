b542c12cd45e0e8cea03dece3255ae5fb75b99d5
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-11-05 06:23:43 -0700

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -48,10 +48,6 @@ void close_file(int fd)
  * @argv: An array of pointers to the arguments.
  *
  * Return: 0 on success.
- * Description: if argument count is incorrect - exit code 97.
- * if file_from doesn't exist or can't be read - exit code 98.
- * if file_to cannot be created or written to - exit code 99.
- * if file_to or file_from cannot be closed - exit code 100.
  */
 
 int main(int argc, char *argv[])
