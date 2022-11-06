1128f3b7689236914a8fc05d17e5d44b3c73df6e
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-11-06 02:28:02 -0800

    Modify 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_numbers - Print numbers from 0-9.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 
 void print_numbers(void)
@@ -12,6 +12,6 @@ void print_numbers(void)
 
 	for (num = 0; num <= 9; num++)
 		_putchar((num % 10) + '0');
-	
+
 	_putchar('\n');
 }
