9b2f53001dd2eff3b7defe5daa11622e5c3e02c5
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-11-06 04:17:44 -0800

    Modify 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,4 +9,23 @@
 
 void print_triangle(int size)
 {
+	int block, index;
 
+	if (size > 0)
+	{
+		for (block = 1; block <= size; block++)
+		{
+			for (index = size - block; index > 0; index--)
+				_putchar(' ');
+
+			for (index = 0; index < block; index++)
+				_putchar('#');
+
+			if (block == size)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+	_putchar('\n')
+}
