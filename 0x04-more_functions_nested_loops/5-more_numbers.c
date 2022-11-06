f4fb484bb043313fac8de1e4b6b1df2d6116f700
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2022-11-06 02:48:33 -0800

    Modify 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,11 +8,16 @@
 
 void more_numbers(void)
 {
-	int i;
+	int num, count;
 
-	for (i = 0; i <= 14; i++)
+	for (count = 0; count <= 9; count++)
 	{
-		_putchar((i) * 10);
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
