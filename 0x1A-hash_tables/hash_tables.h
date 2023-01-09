8a59f6badb7cd092ffefdcbf6d3078ebb93558dc
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-01-08 21:47:49 -0800

    Edit header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -50,9 +50,9 @@ void hash_table_print(const hash_table_t *ht);
 /* 6-hash_table_delete.c */
 void hash_table_delete(hash_table_t *ht);
 /* 100-sorted_hash_table.c */
-shash_table_t *shash_table_create(unsigned long int size);
-shash_node_t *add_n_shash(shash_node_t **, const char *, const char *);
-void add_i_shash(shash_table_t *ht, shash_node_t *new);
+hash_table_t *hash_table_create(unsigned long int size);
+hash_node_t *add_n_shash(hash_node_t **, const char *, const char *);
+void add_i_hash(hash_table_t *ht, shash_node_t *new);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
