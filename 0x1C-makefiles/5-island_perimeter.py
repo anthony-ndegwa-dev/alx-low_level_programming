b6a2c4fee0690a0352e5245af2c1a1457df3fc72
Anthony-ndegwa-dev <Anthony.n.ndegwa@gmail.com>
2023-02-23 05:08:04 -0800

    Function that returns the perimeter of island described in grid

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Function returning perimeter island described in grid"""
+
+
+def island_perimeter(grid):
+    """ Function that returns the perimeter of island described in grid """
+    perimeter = 0
+
+    nrows = len(grid)
+
+    if grid != []:
+        ncolumns = len(grid[0])
+
+    for i in range(nrows):
+        for j in range(ncolumns):
+            if grid[i][j] == 1:
+                if (i - 1) == -1 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if (i + 1) == nrows or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if (j - 1) == -1 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if (j + 1) == ncolumns or grid[i][j + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
